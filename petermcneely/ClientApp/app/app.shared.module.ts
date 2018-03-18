eaf009e266baed0bdaafbb6e02741d7e5fd83644
Peter McNeely <pete.mcneely@gmail.com>
2018-03-18 19:05:07 -0400

    Add Resume.

--- petermcneely/ClientApp/app/app.shared.module.ts
+++ petermcneely/ClientApp/app/app.shared.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ResumeComponent } from './components/resume/resume.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
         AboutComponent,
-        HomeComponent
+        HomeComponent,
+        ResumeComponent
     ],
     imports: [
         CommonModule,
@@ -24,6 +26,7 @@ import { AboutComponent } from './components/about/about.component';
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'about', component: AboutComponent, data: { title: 'About' } },
+            { path: 'resume', component: ResumeComponent, data: { title: 'Resume' } },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ petermcneely/ClientApp/app/components/resume/resume.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'resume',
+    templateUrl: './resume.component.html'
+})
+
+export class ResumeComponent {
+}
